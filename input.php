beeb95380a3b21260ee92a3206218e5b4f4772b8
Artem Leonov <aleonov@tepsvet.ru>
2016-12-02 18:50:36 +0300

    LSB Позднее статическое связывание

--- input.php
+++ input.php
@@ -1,14 +1,32 @@
 <?php
-/*
+/*  https://habrahabr.ru/post/259627/
 Ключевое слово self в PHP всегда значит «имя класса, где это слово написано».
  В данном случае self заменяется на класс Model, а self::$table — на Model::$table.
 Такая языковая возможность называется «ранним статическим связыванием».
+
+Теперь вы понимаете, почему PHP ведёт себя в этой ситуации неинтуитивно.
+self был связан с классом Model тогда, когда о классе User еще ничего не было известно, поэтому и указывает на Model.
 */
 class Model
 {
     public static $table='table';
     public static function getTable(){
-        return self::$table;
+        echo '<p>Текущий класс: '.get_called_class().'</p>';
+        return self::$table;                                    //Заменили self на static
+
     }
 }
-echo Model::getTable();
+class User extends Model
+{
+    public static $table='users';
+}
+echo Model::getTable();     //table
+echo '<br>';
+echo User::getTable();      //table
+/*
+     LSB (Late Static Binding).
+Для решения этой дилеммы был придуман механизм связывания «позднего», на этапе рантайма.
+Работает он очень просто — достаточно вместо слова «self» написать «static» и связь будет установлена с тем классом,
+который вызывает данный код, а не с тем, где он написан:
+*/
+
